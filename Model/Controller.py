6766c9d74fc16928b25b010f15ff2627c602aee7
romano <docrho78@gmail.com>
2022-02-08 04:36:35 -0400

    view finished for showing turn

---
+++ Model/Controller.py
@@ -0,0 +1,248 @@
+from Model import View
+from Model.Db import DbManager
+from Model.Player import Player
+from Model.Tournament import Tournament
+import json
+
+v = View.Views()
+db = DbManager()
+# function######
+
+
+def home():
+    v.load_page("home")
+    response = str(input())
+    return response
+
+# starting app####
+
+def launch():
+
+    while True:
+
+        responsemenu = home()
+        if responsemenu == "0":  # To quit
+            break
+
+        elif responsemenu == "1":  # load tournament
+            tournament = Tournament()
+            player = Player()
+            # store the response on variable tournament_id
+
+            tournament_id = int(v.load_page("_update_tournament_menu_prompt"))
+            # checking if the tournament id exist
+            if tournament.tournament_id_checking(tournament_id):
+                # adding tournament from database on tournament instance
+                tournament.tournament_instance(tournament.get_tournament_by_id(
+                    tournament_id)
+                )
+                # store starting time
+                tournament.current_tour.start_time.append(
+                    tournament.current_tour.current_datetime()
+                )
+                # show players on console
+                v.load_page("show_player_on_tournament", tournament)
+                v.load_page("list_tournament", tournament)
+                # sort player instance in tournament instance
+                tournament.sort_player_by_rank()
+
+                # starting first tour with player instance from tournament
+                tournament.current_tour.tour1(tournament.players)
+                # ask view to type score of the first tour
+                score = v.load_page("add_score_to_match",
+                                    tournament.current_tour.match_list)
+                # adding score to match list of tuple
+                tournament.current_tour.add_score_to_match(score)
+                # store current tour on tour list un tournament
+                tournament.store_match_already_played()
+                tournament.adding_score_to_players_instance_from_match()
+                # store end time of turn
+                tournament.current_tour.end_time.append(
+                    tournament.current_tour.current_datetime()
+                )
+                # asking for rank change
+                if v.load_page("do_you_want_modify_rank"):
+                    tournament.players = v.load_page(
+                        "players_modify_rank", tournament.players)
+                ###############################
+                #asking for saving tournament score in actual state
+                if v.load_page("do_you_want_save_tournament"):
+                    v.load_page("update_all_data_from_tournament",
+                                db.update_all_data_from_tournament(
+                                    tournament_id, tournament)
+                                )
+                # starting second turn and other
+
+                for seq in range(3):
+
+                    tournament.sort_player_by_points()
+                    if tournament.check_if_same_points():
+                        tournament.sort_player_by_rank()
+                    else:
+                        pass
+                    tournament.current_tour.tour2(tournament.players)
+                    score = v.load_page("add_score_to_match",
+                                        tournament.current_tour.match_list)
+                    tournament.current_tour.add_score_to_match(score)
+                    tournament.store_match_already_played()
+                    tournament.adding_score_to_players_instance_from_match()
+                    # store end time of turn
+                    tournament.current_tour.end_time.append(
+                        tournament.current_tour.current_datetime()
+                    )
+                    # asking for rank change
+                    if v.load_page("do_you_want_modify_rank"):
+                        tournament.players = v.load_page(
+                            "players_modify_rank", tournament.players)
+                v.load_page("update_all_data_from_tournament",
+                            db.update_all_data_from_tournament(
+                                tournament_id, tournament)
+                            )
+                if v.load_page("do_you_want_save_tournament"):
+                    v.load_page("update_all_data_from_tournament",
+                                db.update_all_data_from_tournament(
+                                    tournament_id, tournament)
+                                )
+
+        elif responsemenu == "2":  # create new tournament
+            tournament = Tournament()
+            player = Player()
+            # store on attribute all player from database
+            list_players = Player.list_all_players()
+            player.all_players = list_players
+            # we are displaying all player ,like this we can choose them by id
+            v.load_page("display_all_players", list_players)
+            # return the 8 player number prompt that we want to select
+            player_id_list = v.load_page("create_tournament_players")
+            # checking up if the id prompted exist
+            if player.players_id_checking(player_id_list):
+                # this method add player and serialize them
+                # verifier les id dans append, la methode static
+                player.append_player_from_id(player_id_list)
+                # calling view for ask tournament name prompt ....
+                tournament_info = v.create_tournament()
+                # creating tournament with tournament method
+                tournament.add_tournament_info(tournament_info,
+                                               player.players_list)
+                # store tournament on database
+                db.store_tournament(tournament)
+            else:
+                v.load_page("error", "player_id")
+
+        elif responsemenu == "3":  # add a player on Player database
+            # init a player that we will send it to view
+            player_prompt = v.load_page("add_player_view", Player())
+            # Taking all player data to compare them with current player,
+            # its for avoiding double
+            # instancing the deserialized data
+            list_all_players = Player.list_all_players()  # static
+            # add_player return True if there is no double on database
+            added_bol = db.add_player(list_all_players, player_prompt)
+            # load a page to print successfull or not
+            v.load_page("player_successfully_added_or_not",
+                        added_bol,
+                        player_prompt
+                        )
+
+        elif responsemenu == "4":  # remove a player on Player database
+            player_to_remove = v.remove_player(Player())
+            # player is list of string from view containing date and name
+            if db.remove_players(player_to_remove.lastname,  # player.remove
+                                 player_to_remove.birth_date):
+                v.load_page("success", "player_removed")
+            else:
+                v.load_page("error", "player_not_removed")
+
+        elif responsemenu == "5":  # list all players from Player database
+
+            player = Player()
+            v.load_page("display_all_players", player.list_all_players())
+
+        elif responsemenu == "6":  # Remove a tournament
+            tournament = Tournament()
+            # remove from id
+            ###########################
+            if tournament.remove_tournament(v.response_input()):
+                v.load_page("success", "tournament_removed")
+            else:
+                v.load_page("error", "tournament_not_removed")
+
+        elif responsemenu == "7":  # List all tournament
+            tournament = Tournament()
+            tournament.list_all_tournament()  # static
+            v.load_page("list_tournament", tournament.all_tournament_list)
+
+        elif responsemenu == "8":  # update a tournament
+            tournament = Tournament()
+            # store the response on variable tournament_id
+            tournament_id = int(v.load_page("_update_tournament_menu_prompt"))
+            # checking if the tournament id exist
+            if tournament.tournament_id_checking(tournament_id):
+                # adding tournament from database on tournament instance
+                tournament.tournament_instance(tournament.get_tournament_by_id(
+                    tournament_id)
+                )
+                v.load_page("list_tournament", tournament)
+                while responsemenu != "6":  # condition to exit the loop
+                    v.load_page("update_tournament_menu")
+                    responsemenu = v.basic_input()
+                    if responsemenu == "1":  # change name condition
+                        # passing the id prompted to the method
+                        db.update_tournament(
+                            "name", tournament_id,
+                            v.load_page("change_tournament_prompt"))
+                    if responsemenu == "2":
+                        # passing the id prompted to the method
+                        db.update_tournament(
+                            "place", tournament_id,
+                            v.load_page("change_tournament_prompt"))
+                    if responsemenu == "3":
+                        # passing the id prompted to the method
+                        db.update_tournament(
+                            "date", tournament_id,
+                            v.load_page("change_tournament_prompt"))
+                    if responsemenu == "4":
+                        # passing the id prompted to the method
+                        db.update_tournament(
+                            "description", tournament_id,
+                            v.load_page("change_tournament_prompt"))
+                    if responsemenu == "5":
+                        tournament.remove_tournament(tournament_id)
+
+            else:
+                v.load_page("error", "tournament_id")
+        elif responsemenu == "9":
+            tournament = Tournament()
+            tournament.list_all_tournament()
+            v.load_page("list_tournament", tournament.all_tournament_list)
+            print('choose the tournament id')
+            tournament_id = int(v.load_page("_update_tournament_menu_prompt"))
+            # checking if the tournament id exist
+            if tournament.tournament_id_checking(tournament_id):
+                # adding tournament from database on tournament instance
+                tournament.tournament_instance(tournament.get_tournament_by_id(
+                    tournament_id)
+                )
+                v.load_page("display_all_players", tournament.players)
+
+        elif responsemenu == "10":  # List round from tournament
+            tournament = Tournament()
+            player = Player()
+            # store the response on variable tournament_id
+
+            tournament_id = int(v.load_page("_update_tournament_menu_prompt"))
+            # checking if the tournament id exist
+            if tournament.tournament_id_checking(tournament_id):
+                # adding tournament from database on tournament instance
+                tournament.tournament_instance(tournament.get_tournament_by_id(
+                    tournament_id)
+                )
+            else:
+                break
+            # deserialising round
+            tournament.deserialise_round_list()
+            # sending round to view
+            v.load_page("display_all_round_or_match_from_tournament",
+                        tournament.rounds_list
+                        )
+
---
+++ Model/View.py
@@ -0,0 +1,322 @@
+
+class Views:
+    def start_match(self):
+        print("Type 1 to start match or 0 to leave")
+        return input()
+
+    def remove_player(self, player):
+        print("Type the Lastname of the player that you want to delete")
+        player.lastname = input()
+        print("Type the date of birth of the player that you want to delete")
+        player.birth_date = input()
+        return player
+
+    def player_successfully_added_or_not(self, added, player):
+        if added:
+
+            print(f"The player {player.lastname} "
+                  f"{player.first_name} was added !!!\n")
+        else:
+            print("Player already exist, so not added")
+
+    def add_player_view(self, player):
+        print("You can now add a player\n")
+        print("Type his first name:\n")
+        player.first_name = input()
+        print("his lastname\n")
+        player.lastname = input()
+        print("his birth date\n")
+        player.birth_date = input()
+        print("his gender\n")
+        player.gender = input()
+        print("his rank \n")
+        player.ranking = input()
+
+        return player
+
+    def success(self, success):
+        if success == "player_added":
+            print('Players added successfully ')
+        if success == "player_removed":
+            print("Player removed successfully")
+        if success == "tournament_removed":
+            print("Tournament removed successfully")
+
+    def display_all_players(self, all_players):
+        print("Type 1 to display players by alphabetical order\n"
+              "Type 2 to display players by ranking point")
+        response = int(input())
+        player_list = []
+        if response == 1:
+            for player in all_players:
+                player_list.append(f"{player.lastname} "
+                                   f"{player.first_name} date :"
+                                   f" {player.birth_date}\n"
+                                   f" Rank : {player.ranking} id :"
+                                   f" {player.doc_id}"
+                                   f" Points : {player.point}"
+                                   )
+            sorted_list = sorted(player_list, key=str.lower)
+            print("List of players by alphabetical order\n")
+            for player in sorted_list:
+                print(player)
+            print("\n")
+
+        elif response == 2:
+            for player in all_players:
+                player_list.append(f"{player.ranking} {player.lastname} "
+                                   f"{player.first_name} "
+                                   f"date :{player.birth_date}"
+                                   f" id : {player.doc_id}"
+                                   )
+            sorted_list = sorted(player_list)
+            print("List of players by ranking order\n")
+            for player in sorted_list:
+                print(player)
+            print("\n")
+
+    def display_all_players_from_tournament(self, all_players):
+        print("Player list:\n")
+        for player in all_players:
+            print(f"{player.lastname} "
+                  f"{player.first_name} {player.birth_date}"
+                  f" {player.ranking}"
+                  )
+        print("\n")
+
+    def _error(self, error_name):
+        if "player_name_exist" in error_name:
+            print("le player name already exist\n")
+
+        if "tournament_id" in error_name:
+            print("The tournament id was not found!!\n")
+        if "player_id" in error_name:
+            print("Error on player id")
+        if "player_not_removed" in error_name:
+            print("Player not removed")
+        if "tournament_not_removed" in error_name:
+            print("Tournament not removed")
+
+    def response_input(self):
+        print('tapez le numero du tournois a supprimer')
+        return input()
+
+    def _base_menu(self):
+        print("Hi, Welcome to tournament chess app!\nType "
+              "\n1 to load a tournament "
+              "\n2 to create new tournament "
+              "\n3 to add a player "
+              "\n4 to remove a player "
+              "\n5 to list all players "
+              "\n6 to remove a tournament "
+              "\n7 to list all tournaments "
+              "\n8 update tournament "
+              "\n9 List all player from tournament   "
+              "\n10 List all round from tournament   "
+              "\n0 to quit the app")
+
+    def _tournaments_displays(self):
+        print("ont load le tournament")
+
+    def list_tournaments(self, tournaments):
+        if isinstance(tournaments, list):
+            for tournament in tournaments:
+
+                print(
+                    "tournament name ::  " + tournament.name,
+                    "\ntournament place ::  " + tournament.place,
+                    "\ntournament date ::  " + tournament.date,
+                    "\ntournament description ::  " + tournament.description,
+                    "\n tournament id ::  " + str(tournament.doc_id),
+                    "\n----------------------------------------"
+                    )
+        else:
+            print(
+                "tournament name ::  " + tournaments.name,
+                "\ntournament place ::  " + tournaments.place,
+                "\ntournament date ::  " + tournaments.date,
+                "\n tournament id ::  " + str(tournaments.doc_id),
+                "\n----------------------------------------"
+            )
+
+    def _create_tournament_players(self):
+        choice_list = []
+        print(
+            "\nWelcome to the tournament creator\n"
+            "Please select your 8 players with her id displayed above\n"
+            )
+        i = 1
+        while i <= 8:
+            print(f"Type the number of player number {i} !")
+            choice_list.append(input())
+            i += 1
+        return choice_list
+
+    def create_tournament(self):
+        tournament_info = {}
+        print("Type the tournament name")
+        tournament_info["name"] = input()
+        print("Type the tournament place")
+        tournament_info["place"] = input()
+        print("Type the tournament date")
+        tournament_info["date"] = input()
+        print("Type the tournament time")
+        tournament_info["time"] = input()
+        print("Type the tournament description")
+        tournament_info["description"] = input()
+
+        return tournament_info
+
+    def _update_tournament_menu(self):
+        print(
+            "\n1 to change name of tournament",
+            "\n2 to change place of tournament",
+            "\n3 to change date of tournament",
+            "\n4 to change description of tournament",
+            "\n5 to delete tournament",
+            "\n6 to return on main menu",
+              )
+
+    def _update_tournament_menu_prompt(self):
+        print("Type the tournament number that you want to select")
+        return input()
+
+    def change_tournament_prompt(self):
+        print("You can input the change now")
+        return input()
+
+    def basic_input(self):
+        return input()
+
+    def add_score_to_match(self, tour: list):
+        score_list = []
+        match_list = list(tour)
+        for match in match_list:
+            print(f"Entrez le score pour ce match \n {match}")
+            score_list.append(float(input()))
+            score_list.append(float(input()))
+        return score_list
+
+    def players_modify_rank(self, players):
+        i = 0
+        for player in players:
+            print(f"id : {i} {player.first_name} {player.lastname}"
+                  f" current rank is : {player.ranking}")
+            i += 1
+        print("Choose your player by tiping his id")
+        response = int(input())
+        if(response < 0) or (response > 7):
+            while (response < 0) or (response > 7):
+                print("Bad id choose again")
+                response = int(input())
+        print(f"Okay you choose {players[response].lastname}"
+              f" {players[response].first_name}")
+        print("Type the rank that you want to atribute")
+        players[response].ranking = input()
+        print("The ranking is updated successfully")
+        return players
+
+    def do_you_want_modify_rank(self):
+        print("Hi The turn is finished"
+              " do you want to modify a rank from a player?\n"
+              "If yes Type 1 otherwise type something else")
+        response = str(input())
+        if response == "1":
+            return True
+        else:
+            return False
+
+    def show_player_on_tournament(self, tournament):
+        print("This is the list of players playing in this tournament")
+        print("---------------------------------------\n")
+        for player in tournament.players:
+            print(f"{player.lastname} {player.first_name}")
+            print(f"current rank: {player.ranking} point: {player.point}")
+        print("---------------------------------------\n")
+        return True
+
+    def update_all_data_from_tournament(self, response):
+        if response:
+            print("tournament updated correctly")
+        else:
+            print("error while updating tournament")
+        return response
+
+    def do_you_want_save_tournament(self):
+        print("Do you want save the tournament in actual state ?")
+        print("Type 1 to say yes , type other key to say no")
+        response = int(input())
+        if response == 1:
+            return True
+        else:
+            return False
+
+    def display_all_round_or_match_from_tournament(self,round_list):
+        i = 0
+        x = 1
+        r = 1 #  round
+        t = 1 # turn
+        for round in round_list:
+            print(f"match number {r} :")
+            print(round[i]["lastname"] + " " + round[i]["first_name"] +
+                  " point " + round[x]
+                  )
+            print(round[i+2]["lastname"] + " " + round[i+2]["first_name"] +
+                  " point " + round[x+2]
+                  )
+            if (r % 4) == 0:
+                print("-------------------------")
+                print(f"Fin du tour numero {t}")
+                print("-------------------------")
+                t += 1
+            r += 1
+
+    def load_page(self, page_name: str, *args,):
+        if page_name == "home":
+            self._base_menu()
+        elif page_name == "display_tournament":
+            self._tournaments_displays()
+        elif page_name == "create_tournament_players":
+            return self._create_tournament_players()
+        elif page_name == "list_tournament":
+            self.list_tournaments(args[0])
+
+        elif page_name == "update_tournament_menu":
+            self._update_tournament_menu()
+        elif page_name == "_update_tournament_menu_prompt":
+            return self._update_tournament_menu_prompt()
+        elif page_name == "change_tournament_prompt":
+            return self.change_tournament_prompt()
+        elif page_name == "add_player_view":
+            return self.add_player_view(args[0])
+        elif page_name == "player_successfully_added_or_not":
+            # arg[0] is a boolean if added or not, arg[1] is player data
+            return self.player_successfully_added_or_not(args[0], args[1])
+        elif page_name == "remove_player":
+            return self.remove_player(args[0])
+        elif page_name == "display_all_players":
+            return self.display_all_players(args[0])
+        elif page_name == "display_all_players_from_tournament":
+            return self.display_all_players_from_tournament(args[0])
+        elif page_name == "create_tournament":
+            return self.create_tournament()
+        elif page_name == "success":
+            return self.success(args[0])
+        elif page_name == "add_score_to_match":
+            return self.add_score_to_match(args[0])
+        elif page_name == "players_modify_rank":
+            return self.players_modify_rank(args[0])
+        elif page_name == "do_you_want_modify_rank":
+            return self.do_you_want_modify_rank()
+        elif page_name == "show_player_on_tournament":
+            return self.show_player_on_tournament(args[0])
+        elif page_name == "update_all_data_from_tournament":
+            return self.update_all_data_from_tournament(args[0])
+        elif page_name == "do_you_want_save_tournament":
+            return self.do_you_want_save_tournament()
+        elif page_name == "display_all_round_or_match_from_tournament":
+            return self.display_all_round_or_match_from_tournament(args[0])
+        elif page_name == "basic_input":
+            return self.basic_input()
+        elif page_name == "error":
+            self._error(args)
